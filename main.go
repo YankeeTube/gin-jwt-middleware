7dbd0b5bc7d0d1c2955586428e84e7bcce3ff59e
pypi <subyankee@gmail.com>
2020-05-22 13:15:42 +0900

    first

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"crypto/rsa"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func TokenAuthMiddleware(c *gin.Context) {
+	publicKey := c.MustGet("PRIVKEY")
+	token, err := c.Request.Cookie("access-token")
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"status": http.StatusUnauthorized,
+			"error":  "Authentication failed",
+		})
+		c.Abort()
+		return
+	}
+
+	tokenString := token.Value
+	if tokenString == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"code":    http.StatusUnauthorized,
+			"message": "Token is None",
+		})
+		c.Abort()
+		return
+	}
+
+	claims := &jwt.MapClaims{}
+
+	_, err = jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return publicKey, nil
+	})
+	fmt.Println(err)
+
+	if err == jwt.ErrSignatureInvalid {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":    http.StatusUnauthorized,
+			"message": "Invalid Signature",
+		})
+		c.Abort()
+	} else if v, _ := err.(*jwt.ValidationError); v != nil && v.Errors == jwt.ValidationErrorExpired {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"code":    http.StatusUnauthorized,
+			"message": "Token is Expired",
+		})
+		c.Abort()
+	} else if err != nil && err.Error() == rsa.ErrVerification.Error() {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"code":    http.StatusUnauthorized,
+			"message": "Verification failed",
+		})
+		c.Abort()
+	} else if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"code":    http.StatusUnauthorized,
+			"message": "Authentication failed",
+		})
+		c.Abort()
+	} else {
+		c.Next()
+	}
+}
